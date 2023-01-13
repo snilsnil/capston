db74731270f1bff89abaaca23be9b1bc60d9827b
snilsnil <heo6601@naver.com>
2023-01-14 00:02:59 +0900

    전체삭제

--- js/mobilemain.js
+++
@@ -1,8 +0,0 @@
-$('#slide3>ul>li').hide();
-$('#slide3>ul>li:first-child').show();
-
-setInterval(function(){ 
-	$('#slide3>ul>li:first-child').fadeOut() 
-	.next().fadeIn().end(6000) 
-	.appendTo('#slide3>ul');
-},6000);
